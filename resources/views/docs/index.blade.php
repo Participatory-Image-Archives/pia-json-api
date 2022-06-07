55cacd804175c73fa3fc0a65c0b8e91e94fdb76f
Adrian Demleitner <adrian@thgie.ch>
2022-06-07 15:35:51 +0200

    fixed path to swagger assets

--- resources/views/docs/index.blade.php
+++ resources/views/docs/index.blade.php
@@ -4,16 +4,16 @@
   <head>
     <meta charset="UTF-8">
     <title>Swagger UI</title>
-    <link rel="stylesheet" type="text/css" href="{{ asset('docs/swagger-ui.css') }}" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('docs/index.css') }}" />
-    <link rel="icon" type="image/png" href="{{ asset('docs/favicon-32x32.png') }}" sizes="32x32" />
-    <link rel="icon" type="image/png" href="{{ asset('docs/favicon-16x16.png') }}" sizes="16x16" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('swagger/swagger-ui.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('swagger/index.css') }}" />
+    <link rel="icon" type="image/png" href="{{ asset('swagger/favicon-32x32.png') }}" sizes="32x32" />
+    <link rel="icon" type="image/png" href="{{ asset('swagger/favicon-16x16.png') }}" sizes="16x16" />
   </head>
 
   <body>
     <div id="swagger-ui"></div>
-    <script src="{{ asset('docs/swagger-ui-bundle.js') }}" charset="UTF-8"> </script>
-    <script src="{{ asset('docs/swagger-ui-standalone-preset.js') }}" charset="UTF-8"> </script>
-    <script src="{{ asset('docs/swagger-initializer.js') }}" charset="UTF-8"> </script>
+    <script src="{{ asset('swagger/swagger-ui-bundle.js') }}" charset="UTF-8"> </script>
+    <script src="{{ asset('swagger/swagger-ui-standalone-preset.js') }}" charset="UTF-8"> </script>
+    <script src="{{ asset('swagger/swagger-initializer.js') }}" charset="UTF-8"> </script>
   </body>
 </html>
